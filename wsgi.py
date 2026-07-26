6d47434da391d34605eb4026cb42c58395d06975
Weather Bot User <weatherbot@example.com>
2026-07-26 03:45:55 -0600

    Make wsgi entrypoint add project root to sys.path

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,10 @@
+import os
+import sys
+
+ROOT = os.path.dirname(os.path.abspath(__file__))
+if ROOT not in sys.path:
+    sys.path.insert(0, ROOT)
+
 from app import app
 
 if __name__ == "__main__":
